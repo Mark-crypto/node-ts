a5d50eb4b61156bae88f9292418f4814c24de2ee
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-05-22 22:55:22 +0300

    add: Create controllers for my API

--- src/controllers/cars.ts
+++ src/controllers/cars.ts
@@ -1,5 +1,17 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
-export const getCars = (req:Request,res:Response)=>{
-res.send("Many cars")
-}
+export const getCars = (req: Request, res: Response) => {
+  res.send("Many cars");
+};
+export const getCar = (req: Request, res: Response) => {
+  res.send("Single car");
+};
+export const getCarPaginated = (req: Request, res: Response) => {
+  res.send("Filter cars you get");
+};
+export const getCarHeaders = (req: Request, res: Response) => {
+  res.send("Get headers only");
+};
+export const postCarsSimulation = (req: Request, res: Response) => {
+  res.send("Test post on data not persisted");
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getCars } from "../controllers/cars";
+import {
+  getCar,
+  getCarHeaders,
+  getCarPaginated,
+  getCars,
+  postCarsSimulation,
+} from "../controllers/cars";
 
 const router = Router();
 
 router.get("/cars", getCars);
+router.get("/cars/:id", getCar);
+router.get("/cars/filter", getCarPaginated);
+router.get("/cars/headers", getCarHeaders);
+router.post("/cars", postCarsSimulation);
 
 export default router;
