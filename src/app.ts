72c36214f38d0129c00ab951994dbda56952ff14
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-06-06 00:01:56 +0300

    add: Setup express rate limit and cors to the api

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import express from "express";
 import indexRouter from "./routes/index";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 5001;
+const limit = rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000,
+  message: "To many requests.Try again later",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
+app.use(limit);
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use("/api", indexRouter);
 
 app.listen(PORT, () => {
