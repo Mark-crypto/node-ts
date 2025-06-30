302d9ac6e4bf6e8b6973fb1a8294857018945931
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-06-30 15:43:42 +0300

    add: Add winston as a logger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,24 @@ import {
   getCars,
   postCarsSimulation,
 } from "../controllers/cars";
+import winston from "winston";
+
+const { combine, timestamp, printf } = winston.format;
+const logger = winston.createLogger({
+  level: "debug",
+  // format: winston.format.json(),
+  format: combine(
+    timestamp(),
+    printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "app.log", level: "error" }),
+  ],
+});
+
+logger.info("An info log");
+logger.error("An error log");
 
 const router = Router();
 
